b56088838579cd5fdb4dd468f5b3ee60da644f18
IanMacDonnacha <Ian.macdonnacha@gmail.com>
2023-02-16 20:43:06 +0000

    Create loop and get user guess

---
+++ battleship.js
@@ -0,0 +1,13 @@
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+
+let guess;
+let hits = 0;
+let guesses = 0;
+
+let isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number between 0-6) :")
+}
