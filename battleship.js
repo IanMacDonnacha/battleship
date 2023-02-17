b4f013765d582146a00c3b392e0186b5b61cf327
IanMacDonnacha <Ian.macdonnacha@gmail.com>
2023-02-17 20:57:04 +0000

    Add IF hits == 3 to alert user hit  else alert miss, with final stats at finish

--- battleship.js
+++ battleship.js
@@ -9,15 +9,26 @@ let guesses = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-    guess = prompt("Ready, aim, fire! (enter a number between 0-6) :")
+    guess = prompt("Ready, aim, fire! (enter a number between 0-6) :");
     if (guess < 0 || guess > 6) {
-        alert("please enter a valid cell number!")
-    }else {
-        guesses = guesses + 1
+        alert("please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
 
         if (guess == location1 || location2 || location3){
             hits = hits + 1;
+            alert("HIT!");
+            if (hits == 3) {
+                isSunk == true;
+                alert("You sank my battleship!");
+            }
+        } else {
+            alert("MISS!");
         }
 
     }
 }
+
+lets stats = `You took ${guess} guesses to sink the battleship, which means your shooting accuracy was ${3/guesses}`;
+
+alert(stats);
